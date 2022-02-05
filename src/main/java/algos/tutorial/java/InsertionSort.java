8841880fc554394c387f6fffe8709a82f17092ae
Shashank Siripragada <siripragada.s@northeastern.edu>
2022-02-04 19:09:59 -0500

    Adding files

---
+++ src/main/java/algos/tutorial/java/InsertionSort.java
@@ -0,0 +1,26 @@
+package algos.tutorial.java;
+import java.util.Arrays;
+
+public class InsertionSort {
+    public int[] sort(int[] nums){
+        int len = nums.length;
+        for (int i=1; i<len; i++){
+            int curValue = nums[i];
+            int prevIndex = i-1;
+
+            while(prevIndex>=0 && curValue<nums[prevIndex]){
+                nums[prevIndex + 1] = nums[prevIndex];
+                prevIndex--;
+            }
+            nums[prevIndex+1] = curValue;
+        }
+        return nums;
+    }
+
+    public static void main(String args[]) {
+        int[] input = {4, 2, 1, 5, 3};
+        InsertionSort ins = new InsertionSort();
+        int[] output = ins.sort(input);
+        System.out.print(Arrays.toString(output));
+    }
+}
