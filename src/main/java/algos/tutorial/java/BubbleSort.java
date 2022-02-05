d47cc6fba0437548aeb27112258edd599297b9f3
Shashank Siripragada <siripragada.s@northeastern.edu>
2022-02-04 21:18:39 -0500

    adding bubble sort comparisons

--- src/main/java/algos/tutorial/java/BubbleSort.java
+++ src/main/java/algos/tutorial/java/BubbleSort.java
@@ -3,25 +3,80 @@ package algos.tutorial.java;
 import java.util.Arrays;
 
 public class BubbleSort {
-    public int[] sort(int[] nums){
-        int len = nums.length;
-        for (int i=1; i<len; i++){
-            int curValue = nums[i];
-            int prevIndex = i-1;
+    public int[] vanillaBubbleSort(int[] nums){
 
-            while(prevIndex>=0 && curValue<nums[prevIndex]){
-                nums[prevIndex + 1] = nums[prevIndex];
-                prevIndex--;
+        long startTime = System.nanoTime();
+
+        int len = nums.length;
+        for (int i=0; i<len-1; i++){
+            for (int j=0; j< len-1; j++){
+                if (nums[j+1]< nums[j]){
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
             }
-            nums[prevIndex+1] = curValue;
         }
+        long endTime = System.nanoTime();
+        System.out.println("vanillaBubbleSort took " + (endTime - startTime) + "ns");
+        return nums;
+
+    }
+    public int[] advBubbleSort(int[] nums){
+
+        long startTime = System.nanoTime();
+
+        int len = nums.length;
+        for (int i=0; i<len-1; i++){
+            for (int j=0; j< (len-1)-i; j++){
+                if (nums[j+1]< nums[j]){
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        long endTime = System.nanoTime();
+        System.out.println("advBubbleSort took " + (endTime - startTime) + "ns");
+        return nums;
+    }
+
+    public int[] advBubbleSortSwap(int[] nums){
+
+        long startTime = System.nanoTime();
+
+        int len = nums.length;
+        boolean swap= false;
+        for (int i=0; i<len-1; i++){
+            for (int j=0; j< (len-1)-i; j++){
+                if (nums[j+1]< nums[j]){
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                    swap = true;
+                }
+            }
+            if (swap==false){
+                break;
+            }
+        }
+        long endTime = System.nanoTime();
+        System.out.println("advBubbleSort with Swap check took " + (endTime - startTime) + "ns");
         return nums;
     }
 
     public static void main(String args[]) {
-        int[] input = {4, 2, 1, 5, 3};
-        BubbleSort ins = new BubbleSort();
-        int[] output = ins.sort(input);
-        System.out.print(Arrays.toString(output));
+        int[] input = {11, 45, 23, 8, 4, 2, 1, 5, 3};
+        BubbleSort bbl = new BubbleSort();
+
+        int[] out = bbl.vanillaBubbleSort(input);
+        System.out.println(Arrays.toString(out));
+
+        out = bbl.advBubbleSort(input);
+        System.out.println(Arrays.toString(out));
+
+        out = bbl.advBubbleSortSwap(input);
+        System.out.println(Arrays.toString(out));
+
     }
 }
